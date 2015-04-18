44aa9cc11f67d8d7dffb392840994a853bdfdfba
Geymerson <geymerson.r@gmail.com>
2015-04-18 19:00:28 -0300

    Sboard Implementations

--- Sudoku/src/SBoard.java
+++ Sudoku/src/SBoard.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Random;
 
 public class SBoard {
 	//Game board
@@ -8,17 +9,22 @@ public class SBoard {
 	private int [][] playerBoard;
 	
 	//Filled cells verifier
-	private int [] filledCells = new int[81];
+	private int [] filledCells;
 	
 	//Sudoku ready board
-	private FileReader sudokuBoard;
+	private BufferedReader sudokuBoard;
 	
-	SBoard() {
-		try {
-			sudokuBoard = new FileReader("../GameFiles/sudokuBoards.txt");
-		}
-		catch(FileNotFoundException e) {
-			System.out.println("File could not be opened!");
+	//private String difficult;
+	
+	SBoard(String difficult) {
+		//Initiate board
+		this.board = new int[9][9];
+		
+		//Set all boards cells to a not filled state "0"
+		this.filledCells = new int[81];
+		
+		for(int i = 0; i < 81; i++) {
+			this.filledCells[i] = 0;
 		}
 	}
 
@@ -44,7 +50,57 @@ public class SBoard {
 		//TODO
 	}
 	
-	public void launchBoard() {
-		//TODO
+	public void launchBoard(String difficult) throws IOException {
+		
+		//File line receiver
+		String line;
+		
+		//Line where the board is located
+		int lineCounter = 0;
+		
+		//Random number generator to pick up
+		//random boards
+		Random randomicNumberGenerator = new Random();
+		
+		//Choose a random "difficult" board
+		//Default difficult (easy 2 ~ 11)
+		int boardNumber =
+				//Generate a random number between (2~11)
+				randomicNumberGenerator.nextInt(10) + 1;
+
+		if(difficult == "Medium") {
+			boardNumber += 11;
+		}
+		else if(difficult == "Hard") {
+			boardNumber += 22;
+		}
+		
+		try {
+			sudokuBoard = new BufferedReader(
+							new FileReader("../GameFiles/sudokuBoards.txt"));
+			while((line = sudokuBoard.readLine()) != null) {
+				if((boardNumber - 1) == lineCounter) {
+					for(int i = 0; i < line.length(); i++) {
+						for(int row = 0; row < 9; row++) {
+							for(int column = 0; column < 9; column++) {
+								board[row][column] = line.charAt(i); 
+							}
+						}
+					}
+				}
+				lineCounter++;
+			}
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("File could not be opened!");
+		}
+		catch(IOException e) {
+			System.out.printf("Exception occurred trying to read %s",
+					sudokuBoard);
+			return;
+		}
+		finally {
+			sudokuBoard.close();
+		}
 	}//End method launchBoard
 }
