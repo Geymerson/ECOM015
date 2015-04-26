5789588ea2c5321c5fa0c73f3ee4bc1d15f2c545
Geymerson <geymerson.r@gmail.com>
2015-04-26 02:31:15 -0300

    SGui Implementation

--- Sudoku/src/SGUI.java
+++ Sudoku/src/SGUI.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import java.awt.Dimension;
@@ -10,17 +11,16 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
 import javax.swing.JComboBox;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.BadLocationException;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class SGUI extends JFrame {
 	private GridBagLayout gameLayout;
 	private GridBagConstraints constraints;
-	private JTextField[] cell;
+	protected JTextField[] cell;
 	private JLabel playerName;
 	private JLabel playerScore;
 	private JButton solveButton;
@@ -31,7 +31,7 @@ public class SGUI extends JFrame {
 	private JButton newGameButton;
 	private Font font;
 	private SBoard gameBoard;
-	
+
 
 	public SGUI() {
 		super("Sudoku");
@@ -42,52 +42,52 @@ public class SGUI extends JFrame {
 		font = new Font("SansSerif", Font.BOLD, 20);
 		cell = new JTextField[81];
 		gameBoard = new SBoard();
-		
+
 		try {
 			gameBoard.launchGameBoard("Hard");
 			gameBoard.launchPlayerBoard();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		playerName = new JLabel("PlayerName");
 		constraints.fill = GridBagConstraints.HORIZONTAL;
 		addComponent(playerName, 0, 0, 1, 1);
-		
+
 		playerScore = new JLabel("PlayerScore");
 		//constraints.fill = GridBagConstraints.VERTICAL;
 		addComponent(playerScore, 12, 0, 2, 1);
-		
+
 		newGameButton = new JButton("New Game");
 		constraints.fill = GridBagConstraints.BOTH;
 		addComponent(newGameButton, 6, 11, 1, 1);
-		
+
 		restartButton = new JButton("Restart Game");
 		//constraints.fill = GridBagConstraints.HORIZONTAL;
 		addComponent(restartButton, 7, 11, 1, 1);
-		
+
 		loadButton = new JButton("Load Game");
 		//constraints.fill = GridBagConstraints.HORIZONTAL;
 		addComponent(loadButton, 8, 11, 1, 1);
-				
+
 		showSolutionButton = new JButton("Show Solution");
+		showSolutionButton.addActionListener(new ButtonHandler());
 		//constraints.fill = GridBagConstraints.HORIZONTAL;
 		addComponent(showSolutionButton, 9, 11, 1, 1);
-		
+
 		solveButton = new JButton("Solve");
+		solveButton.addActionListener(new ButtonHandler());
 		//constraints.fill = GridBagConstraints.BOTH;
 		addComponent(solveButton, 10, 11, 1, 1);
-		
+
 		int counter = 0;
-		
+
 		for(int row = 2; row < 11; row++) {
 			for(int column = 1; column < 10; column ++) {
 				cell[counter] = new JTextField();
 				cell[counter].setPreferredSize(new Dimension(30, 30));
 				cell[counter].setFont(font);
 				cell[counter].setHorizontalAlignment(JTextField.CENTER);
-				cell[counter].getDocument().addDocumentListener(
-						new MyDocumentListener());
 				int playerBoardCell =
 						gameBoard.getCell(row - 2,
 								column - 1,
@@ -105,7 +105,7 @@ public class SGUI extends JFrame {
 			}
 		}
 	}
-	
+
 	private void addComponent(Component component,
 			int row, int column, int width, int height) {
 		constraints.gridx = column;
@@ -117,28 +117,54 @@ public class SGUI extends JFrame {
 		gameLayout.setConstraints(component, constraints);
 		add(component);
 	}
-	
-	private class MyDocumentListener implements DocumentListener {
-		public void changedUpdate(DocumentEvent event) {
-			//Empty method
+
+	private class ButtonHandler implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			if(event.getSource() == solveButton) {
+				int atPosition = 0;
+				for(int row = 0; row < 9; row++) {
+					for(int column = 0; column < 9; column++) {
+						if(cell[atPosition].isEditable() &&
+								(cell[atPosition].getText().length()) == 1){
+							gameBoard.setCell(row,
+									column,
+									Integer.parseInt(cell[atPosition].getText()),
+									gameBoard.getPlayerBoard());
+						}
+						atPosition++;
+					}
+				}
+//				JOptionPane.showMessageDialog(solveButton.getParent(),
+//						String.format("You pressed %s", event.getActionCommand()));
+			}
+			else if (event.getSource() == showSolutionButton) {
+				int atPosition = 0;
+				for(int row = 0; row < 9; row++) {
+					for(int column = 0; column < 9; column++) {
+						if(cell[atPosition].isEditable()){
+							int gameBoardCell =
+									gameBoard.getCell(row,
+											column,
+											gameBoard.getGameBoard());
+							cell[atPosition].setText(Integer.toString(gameBoardCell));
+							
+						}
+						atPosition++;
+					}
+				}
+			}
+			else if (event.getSource() == restartButton) {
+				
+			}
+			else if (event.getSource() == saveButton) {
+
+			}
+			else if (event.getSource() == loadButton) {
+
+			}
+			else if (event.getSource() == newGameButton) {
+
+			}
 		}
-		
-		public void insertUpdate(DocumentEvent event) {
-			JTextField match =  (JTextField) event.getDocument();
-//			GridBagLayout layout = gameLayout;
-//			for (Component comp : getComponents()) {
-//				if(comp == match) {
-//					GridBagConstraints gbc = layout.getConstraints(comp);
-//					int row = gbc.gridx;
-//					int column = gbc.gridy;
-//					System.out.printf("X pos %d, Y pos %d", row, column);
-//					break;
-//				}
-//			}
-	    }
-		
-	    public void removeUpdate(DocumentEvent event) {
-	    	//TODO
-	    }
 	}
 }
